1dd56ec19b08bacea89dcd3ff3f263b49d104904
isisj <1078160@hr.nl>
2024-12-04 11:13:11 +0100

    button changed for contrast reasons, footer added, images added for footer New check added for contrast reasons

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,35 @@
-<footer class="bg-dark-green text-white p-4">
-    <div class="container mx-auto text-center">
-        <p>&copy; {{ date('Y') }} Mijn Merk. Alle rechten voorbehouden.</p>
+<footer class="bg-dark-moss text-cream rounded-t-[30px] p-6">
+    <div class="font-sans flex flex-col lg:flex-row lg:justify-evenly">
+        <!-- Section voor werknemer -->
+        <div class="mb-6 lg:mb-0 lg:flex lg:flex-col lg:items-start text-center lg:text-left">
+            <h2 class="font-bold text-rose-brown text-lg sm:text-xl mb-2">Voor de werknemer</h2>
+            <ul class="space-y-2">
+                <li class="text-lg sm:text-xl"><a href="#">Veel gestelde vragen</a></li>
+                <li class="text-lg sm:text-xl"><a href="#">Alle aangesloten werkgevers</a></li>
+            </ul>
+        </div>
+
+        <!-- Section voor werkgever -->
+        <div class="mb-6 lg:mb-0 lg:flex lg:flex-col lg:items-start text-center lg:text-left">
+            <h2 class="font-bold text-rose-brown text-lg sm:text-xl mb-2">Voor de werkgever</h2>
+            <ul class="space-y-2">
+                <li class="text-lg sm:text-xl"><a href="#">Meld je bedrijf aan!</a></li>
+                <li class="text-lg sm:text-xl"><a href="#">Veel gestelde vragen</a></li>
+                <li class="text-lg sm:text-xl"><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <!-- Social Media -->
+        <div class="flex justify-center lg:justify-start items-center space-x-4">
+            <a href="#" class="block">
+                <img src="/img/linkedin.png" alt="LinkedIn" class="w-6 h-6 sm:w-8 sm:h-8 object-contain">
+            </a>
+            <a href="#" class="block">
+                <img src="/img/facebook.png" alt="Facebook" class="w-6 h-6 sm:w-8 sm:h-8 object-contain">
+            </a>
+            <a href="#" class="block">
+                <img src="/img/instagram.png" alt="Instagram" class="w-6 h-6 sm:w-8 sm:h-8 object-contain">
+            </a>
+        </div>
     </div>
 </footer>
