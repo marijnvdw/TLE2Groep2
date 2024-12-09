734833b1f2774204da755275eeef6b488d37a1b7
abelt <113996197+fefeffefeeffefefe@users.noreply.github.com>
2024-12-09 13:08:10 +0100

    ADDED email duidelijker gemaakt

--- resources/views/emails/registration-confirmation.blade.php
+++ resources/views/emails/registration-confirmation.blade.php
@@ -5,13 +5,13 @@
     <div
         class="bg-dark-moss shadow-lg shadow-dark-moss rounded-[30px] p-8 w-[80vw] max-w-[600px] mx-auto overflow-hidden mb-8">
         <h1 class="text-rose-brown text-3xl sm:text-4xl font-bold mb-6 text-center">
-            Bevestiging van uw registratie
+            Bevestiging van uw aanmelding
         </h1>
         <p class="text-white text-base sm:text-lg text-center mb-6">
-            Uw aanmelding is succesvol ontvangen! Een bevestigingsmail is onderweg naar het door u opgegeven e-mailadres.
+            Uw aanmelding is succesvol ontvangen! Een bevestigingsmail is onderweg naar ht door u opgegeven e-mailadres.
         </p>
         <p class="text-white text-base sm:text-lg text-center mb-6">
-            Volg de instructies in de e-mail om uw registratie compleet te maken. Controleer ook uw spam-map als u de e-mail niet direct ziet.
+            Volg de instructies in de e-mail om uw aanmelding compleet te maken. Controleer ook uw spam-map als u de e-mail niet direct ziet.
         </p>
         <div class="mt-6 text-center">
             <a
